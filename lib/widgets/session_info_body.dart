cc367a56a0248fa044167eb2aef456f092086aad
kamran <kamran8037@gmail.com>
2022-05-09 15:22:11 +0500

    ui fixes

--- lib/widgets/session_info_body.dart
+++ lib/widgets/session_info_body.dart
@@ -15,7 +15,6 @@ class SessionInfoBody extends StatelessWidget {
   final VoidCallback onPauseCharging;
   final VoidCallback onResumeCharging;
 
-
   const SessionInfoBody({
     Key? key,
     required this.energy,
@@ -29,111 +28,126 @@ class SessionInfoBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        const Spacer(
-          flex: 2,
-        ),
-        Column(
-          children: [
-            Stack(
-              alignment: Alignment.bottomRight,
-              children: [
-                Padding(
-                  padding:
-                  const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-                  child: SvgPicture.asset(getChargingSessionIconByState(state)),
-                ),
-                if (state == 'ChargingPausedEVSE' ||
-                    state == 'ChargingPausedEV')
-                  SvgPicture.asset('assets/icons/icon_pausecharging.svg'),
-              ],
-            ),
-            const SizedBox(height: 36),
-            // if (state == 'ChargingPausedEVSE' || state == 'Charging')
-            if (state == ChargingState.charging)
-              SecondaryButton(
-                  title: 'Pause Charging', onPressed: onPauseCharging),
-            if (pauseOrResumeChargingTitle(state) != ChargingState.charging &&
-                pauseOrResumeChargingTitle(state) != '')
-              PrimaryButton(
-                  title: 'Resume Charging', onPressed: onResumeCharging),
-          ],
-        ),
-        const Spacer(
-          flex: 1,
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Text(
-              'STATUS',
-              style: AppTextStyles.heading2,
-            ),
-            const SizedBox(height: 8),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.4,
-              child: Text(
-                chargingStateTitle(state).toUpperCase(),
-                overflow: TextOverflow.ellipsis,
-                maxLines: 3,
-                style: AppTextStyles.heading6,
-              ),
-            ),
-            const SizedBox(height: 50),
-            Text(
-              state == 'Unplugged'
-                  ? 'Last Session'.toUpperCase()
-                  : 'Current Session'.toUpperCase(),
-              style: AppTextStyles.heading2,
-            ),
-            const SizedBox(height: 8),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.4,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        'Energy',
-                        style: AppTextStyles.heading3,
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.1),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.4,
+                height: MediaQuery.of(context).size.height * 0.2,
+                child: Center(
+                  child: Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 12, horizontal: 20),
+                        child: SvgPicture.asset(
+                            getChargingSessionIconByState(state)),
                       ),
-                      SizedBox(height: 4),
-                      Text(
-                        'Duration',
-                        style: AppTextStyles.heading3,
-                      ),
+                      if (state == 'ChargingPausedEVSE' ||
+                          state == 'ChargingPausedEV')
+                        SvgPicture.asset('assets/icons/icon_pausecharging.svg'),
                     ],
                   ),
-                  const Spacer(),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.2,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
+                ),
+              ),
+              const SizedBox(height: 32),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.2,
+                height: MediaQuery.of(context).size.height * 0.1,
+                child: Column(
+                  children: [
+                    if (state == ChargingState.charging)
+                      SecondaryButton(
+                          title: 'Pause Charging', onPressed: onPauseCharging),
+                    if (pauseOrResumeChargingTitle(state) !=
+                            ChargingState.charging &&
+                        pauseOrResumeChargingTitle(state) != '')
+                      PrimaryButton(
+                          title: 'Resume Charging',
+                          onPressed: onResumeCharging),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text(
+                'STATUS',
+                style: AppTextStyles.subTitle4,
+              ),
+              const SizedBox(height: 8),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.4,
+                height: MediaQuery.of(context).size.height * 0.2,
+                child: Text(
+                  chargingStateTitle(state).toUpperCase(),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 3,
+                  style: AppTextStyles.heading6,
+                ),
+              ),
+              const SizedBox(height: 50),
+              Text(
+                state == 'Unplugged'
+                    ? 'Last Session'.toUpperCase()
+                    : 'Current Session'.toUpperCase(),
+                style: AppTextStyles.subTitle4,
+              ),
+              const SizedBox(height: 8),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.4,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
                         Text(
-                          energy.toStringAsFixed(2) + ' kWh',
-                          textAlign: TextAlign.start,
-                          style: AppTextStyles.digitsHeading3,
+                          'Energy',
+                          style: AppTextStyles.heading3,
                         ),
-                        const SizedBox(height: 4),
+                        SizedBox(height: 4),
                         Text(
-                          duration + ' h',
-                          style: AppTextStyles.digitsHeading3,
+                          'Duration',
+                          style: AppTextStyles.heading3,
                         ),
                       ],
                     ),
-                  ),
-                ],
+                    const Spacer(),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.2,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            energy.toStringAsFixed(2) + ' kWh',
+                            textAlign: TextAlign.start,
+                            style: AppTextStyles.digitsHeading3,
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            duration + ' h',
+                            style: AppTextStyles.digitsHeading3,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-        const Spacer(flex: 2),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
