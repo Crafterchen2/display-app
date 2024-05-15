33643873652c2876ec273aacfff442330d9d1076
Crafterchen2 <Decker_ben@outlook.de>
2024-05-15 15:44:13 +0200

    removed unused variable

--- lib/screens/control.dart
+++ lib/screens/control.dart
@@ -207,7 +207,6 @@ class ConfigInfoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
     return Padding(
       padding:
