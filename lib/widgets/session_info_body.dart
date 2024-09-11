e94cd8862029fe1578b75beebc9bb189d8eb125d
Crafterchen2 <Decker_ben@outlook.de>
2024-09-11 13:44:43 +0200

    added / changed explanatory comments

--- lib/widgets/session_info_body.dart
+++ lib/widgets/session_info_body.dart
@@ -133,10 +133,10 @@ class _SessionInfoBodyState extends State<SessionInfoBody> {
   @override
   Widget build(BuildContext context) {
     String currentSliderLabel = widget.current.toStringAsFixed(1);
-    //Change the snapping behavior below is sufficient.
+    //Changing the snapping behavior below is sufficient.
     double snapped = 300; //Width of left side when unsnapped
     double offset = 20; //This is to accommodate Padding
-    double threshold = 180;
+    double threshold = 180; //The minimum width of the right side when snapped
     NumberSnap carSideWidth = NumberSnap(
       parameter: MediaQuery.of(context).size.width - adjustScale(snapped),
       snapped: adjustScale(snapped - offset),
