424f2cc0c5c362617296e6d7108b32ac3a2d04c3
Crafterchen2 <Decker_ben@outlook.de>
2024-09-11 12:29:32 +0200

    removed unnecessary container

--- lib/screens/lan_info_screen.dart
+++ lib/screens/lan_info_screen.dart
@@ -76,147 +76,144 @@ class _LanInfoScreenState extends State<LanInfoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        //Is Container really necessary?
-        color: Theme.of(context).colorScheme.background,
-        child: Stack(
-          children: [
-            _showProgress
-                ? Center(
-                    child: CircularProgressIndicator(
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-                  )
-                : Column(
-                    children: [
-                      SizedBox(height: screenHeight * 0.1),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: _connected
-                                ? Theme.of(context)
-                                    .colorScheme
-                                    .tertiaryContainer
-                                : Theme.of(context).colorScheme.errorContainer,
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.only(
-                                left: screenWidth * 0.2,
-                                right: screenWidth * 0.02,
-                                top: screenHeight * 0.02,
-                                bottom: screenHeight * 0.02),
-                            child: Text(
-                              _connected
-                                  ? 'CONNECTED TO LAN'.tr()
-                                  : 'NO NETWORK FOUND'.tr(),
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .displayLarge
-                                  ?.copyWith(
-                                    color: _connected
-                                        ? Theme.of(context)
-                                            .colorScheme
-                                            .onTertiaryContainer
-                                        : Theme.of(context)
-                                            .colorScheme
-                                            .onErrorContainer,
-                                  ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: ListView.builder(
-                            itemCount: devices.length,
-                            itemBuilder: (builder, index) {
-                              return NetworkDeviceInfoWidget(
-                                info: devices[index],
-                              );
-                            }),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: Container(
-                          color: Theme.of(context)
-                              .colorScheme
-                              .background, //Is this necessary?
-                          height: screenHeight * 0.2,
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: screenWidth * 0.02,
-                                vertical: screenHeight * 0.01),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                SecondaryButton(
-                                  title: 'Close',
-                                  borderColor: Theme.of(context)
-                                      .colorScheme
-                                      .errorContainer,
-                                  textColor: Theme.of(context)
-                                      .colorScheme
-                                      .errorContainer,
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  //width: screenWidth * 0.2,
-                                ),
-                                SizedBox(width: screenWidth * 0.03),
-                                PrimaryButton(
-                                  child: const Text('Add WIFI'),
-                                  style: Theme.of(context)
-                                      .elevatedButtonTheme
-                                      .style
-                                      ?.copyWith(
-                                        backgroundColor:
-                                            MaterialStateProperty.resolveWith(
-                                          (states) => Theme.of(context)
-                                              .colorScheme
-                                              .errorContainer,
-                                        ),
-                                      ),
-                                  onPressed: () {
-                                    Navigator.of(context).pushNamed(
-                                        AppRoutes.wifiSetupScreen,
-                                        arguments: {
-                                          'init': true,
-                                        });
-                                  },
-                                  //width: screenWidth * 0.3,
-                                ),
-                                SizedBox(width: screenWidth * 0.03),
-                                PrimaryButton(
-                                  child: const Text('Done with SETUP'),
-                                  style: Theme.of(context)
-                                      .elevatedButtonTheme
-                                      .style
-                                      ?.copyWith(
-                                        backgroundColor:
-                                            MaterialStateProperty.resolveWith(
-                                          (states) => Theme.of(context)
-                                              .colorScheme
-                                              .tertiaryContainer,
-                                        ),
-                                      ),
-                                  onPressed: () {
-                                    setInitialized();
-                                    Navigator.of(context).pushAndRemoveUntil(
-                                        MaterialPageRoute(builder: (context) {
-                                      return const LandingScreen();
-                                    }), (Route<dynamic> route) => false);
-                                  },
-                                  //width: screenWidth * 0.3,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Stack(
+        children: [
+          _showProgress
+              ? Center(
+                  child: CircularProgressIndicator(
+                    color: Theme.of(context).colorScheme.primary,
                   ),
-          ],
-        ),
+                )
+              : Column(
+                  children: [
+                    SizedBox(height: screenHeight * 0.1),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: _connected
+                              ? Theme.of(context)
+                                  .colorScheme
+                                  .tertiaryContainer
+                              : Theme.of(context).colorScheme.errorContainer,
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: screenWidth * 0.2,
+                              right: screenWidth * 0.02,
+                              top: screenHeight * 0.02,
+                              bottom: screenHeight * 0.02),
+                          child: Text(
+                            _connected
+                                ? 'CONNECTED TO LAN'.tr()
+                                : 'NO NETWORK FOUND'.tr(),
+                            style: Theme.of(context)
+                                .textTheme
+                                .displayLarge
+                                ?.copyWith(
+                                  color: _connected
+                                      ? Theme.of(context)
+                                          .colorScheme
+                                          .onTertiaryContainer
+                                      : Theme.of(context)
+                                          .colorScheme
+                                          .onErrorContainer,
+                                ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                          itemCount: devices.length,
+                          itemBuilder: (builder, index) {
+                            return NetworkDeviceInfoWidget(
+                              info: devices[index],
+                            );
+                          }),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Container(
+                        color: Theme.of(context)
+                            .colorScheme
+                            .background, //Is this necessary?
+                        height: screenHeight * 0.2,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: screenWidth * 0.02,
+                              vertical: screenHeight * 0.01),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              SecondaryButton(
+                                title: 'Close',
+                                borderColor: Theme.of(context)
+                                    .colorScheme
+                                    .errorContainer,
+                                textColor: Theme.of(context)
+                                    .colorScheme
+                                    .errorContainer,
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                //width: screenWidth * 0.2,
+                              ),
+                              SizedBox(width: screenWidth * 0.03),
+                              PrimaryButton(
+                                child: const Text('Add WIFI'),
+                                style: Theme.of(context)
+                                    .elevatedButtonTheme
+                                    .style
+                                    ?.copyWith(
+                                      backgroundColor:
+                                          MaterialStateProperty.resolveWith(
+                                        (states) => Theme.of(context)
+                                            .colorScheme
+                                            .errorContainer,
+                                      ),
+                                    ),
+                                onPressed: () {
+                                  Navigator.of(context).pushNamed(
+                                      AppRoutes.wifiSetupScreen,
+                                      arguments: {
+                                        'init': true,
+                                      });
+                                },
+                                //width: screenWidth * 0.3,
+                              ),
+                              SizedBox(width: screenWidth * 0.03),
+                              PrimaryButton(
+                                child: const Text('Done with SETUP'),
+                                style: Theme.of(context)
+                                    .elevatedButtonTheme
+                                    .style
+                                    ?.copyWith(
+                                      backgroundColor:
+                                          MaterialStateProperty.resolveWith(
+                                        (states) => Theme.of(context)
+                                            .colorScheme
+                                            .tertiaryContainer,
+                                      ),
+                                    ),
+                                onPressed: () {
+                                  setInitialized();
+                                  Navigator.of(context).pushAndRemoveUntil(
+                                      MaterialPageRoute(builder: (context) {
+                                    return const LandingScreen();
+                                  }), (Route<dynamic> route) => false);
+                                },
+                                //width: screenWidth * 0.3,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+        ],
       ),
     );
   }
