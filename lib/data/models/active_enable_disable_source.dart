afc31262fe78725b8f2fdba7e03bd8c43cb47af3
Kai Hermann <kai-uwe.hermann@pionix.de>
2024-10-01 13:48:33 +0200

    Add missing ActiveEnableDisableSource model (#24)
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

---
+++ lib/data/models/active_enable_disable_source.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'active_enable_disable_source.g.dart';
+
+@JsonSerializable()
+class ActiveEnableDisableSource {
+  final int priority;
+  final String source;
+  final String state;
+
+  ActiveEnableDisableSource(this.priority, this.source, this.state);
+
+  factory ActiveEnableDisableSource.fromJson(Map<String, dynamic> json) => _$ActiveEnableDisableSourceFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ActiveEnableDisableSourceToJson(this);
+}
---
+++ lib/data/models/active_enable_disable_source.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'active_enable_disable_source.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ActiveEnableDisableSource _$ActiveEnableDisableSourceFromJson(Map<String, dynamic> json) => ActiveEnableDisableSource(
+      json['priority'] as int,
+      json['source'] as String,
+      json['state'] as String,
+    );
+
+Map<String, dynamic> _$ActiveEnableDisableSourceToJson(ActiveEnableDisableSource instance) => <String, dynamic>{
+      'priority': instance.priority,
+      'source': instance.source,
+      'state': instance.state,
+    };
